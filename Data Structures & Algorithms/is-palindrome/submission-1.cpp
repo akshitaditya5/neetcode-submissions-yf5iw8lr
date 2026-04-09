e13539f0906f58675d05953d117b554be2f5e4e2
akshitaditya5 <84084287+akshitaditya5@users.noreply.github.com>
2026-04-09 12:14:21 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+      string reversed;
+      for(int i=0;i<s.size();i++){
+        char ch=s[i];
+        if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || 
+        (ch>='0' && ch<='9')){ //'0' and '9' because these are characters not numbers
+        if(ch>='A' && ch<='Z'){
+            ch=ch-'A'+'a';
+        }
+        reversed.push_back(ch);
+        }
+      }  
+      int start=0;
+      int end=reversed.size()-1;
+      while(start<end){
+        if(reversed[start]!=reversed[end]){
+            return false;
+        }
+        start++;
+        end--;
+      }
+      return true;
+      }
+};
