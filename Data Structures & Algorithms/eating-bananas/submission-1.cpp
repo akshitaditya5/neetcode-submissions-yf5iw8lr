cb5aba4c9df4f2467e7f9bd58f791b782196d6a1
akshitaditya5 <84084287+akshitaditya5@users.noreply.github.com>
2026-04-20 12:29:01 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int largestElement(vector<int>& arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.size(); i++)
+            ans = max(ans, arr[i]);
+        return ans;
+    }
+
+    long long totalEatingHours(vector<int>& arr, int hourly) {
+        long long totalHours = 0;
+        for (int i = 0; i < arr.size(); i++)
+            totalHours += ((long long)arr[i] + hourly - 1) / hourly; // ✓ no floating point
+        return totalHours;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = largestElement(piles);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long totalHours = totalEatingHours(piles, mid);
+            if (totalHours <= h)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
